860c82fa98b6617ed28c3c3f08fa4efd310359c4
eric.dantas <ericrommel@gmail.com>
2019-04-15 00:28:20 +0200

    Small structure changes

--- Week01/Challenge/cloud-cost-python/main.py
+++ Week01/Challenge/cloud-cost-python/main.py
@@ -1,9 +1,11 @@
+''' The Weekly challenge of Acceleration Python for Web '''
+
 import calendar
 import datetime
 
-''' The Weekly challenge of Acceleration Python for Web '''
 class CloudCost():
     ''' https://www.codenation.dev/acceleration/aceleradev-python-1/challenge/cloud-cost-python '''
+
     def lambda_execution(self):
         ''' Returns the cost of one function execution '''
 
@@ -22,11 +24,13 @@ class CloudCost():
             - Calculate how many executions was made in a specific month
               NOTE: The execution times is about how many executions was made per day
         '''
+
         days = calendar.monthrange(datetime.datetime.now().year, month_of_year)[1]
         return self.app_execution(execution_times * days)
 
     def year(self, execution_times):
         ''' Returns a list with all total costs per month by a year '''
+
         total_executions = []
         for i in range(1, 13):
             total_executions.append(self.month(execution_times, i))
@@ -35,6 +39,7 @@ class CloudCost():
 
     def time_execution(self):
         ''' Para fins de cálculo vamos considerar que o tempo de execução de cada função é de 3 segundos '''
+
         return 3.0
 
 if __name__ == '__main__':
