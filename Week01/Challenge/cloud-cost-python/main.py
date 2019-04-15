4d6bfd710bdff16373c3490fa6c64f9d3378a6bb
eric.dantas <ericrommel@gmail.com>
2019-04-15 23:36:49 +0200

    The challenged has been done. All testes passed.

--- Week01/Challenge/cloud-cost-python/main.py
+++ Week01/Challenge/cloud-cost-python/main.py
@@ -1,7 +1,8 @@
 ''' The Weekly challenge of Acceleration Python for Web '''
 
-import calendar
 import datetime
+import calendar
+
 
 class CloudCost():
     ''' https://www.codenation.dev/acceleration/aceleradev-python-1/challenge/cloud-cost-python '''
@@ -17,6 +18,7 @@ class CloudCost():
         ''' Returns the total cost of all executions '''
 
         VALUE_BY_MESSAGE = 0.00000040  # Valor por mensagem recebida na fila: U$ 0.00000040
+
         return execution_times * ((self.lambda_execution() * 2) + VALUE_BY_MESSAGE)
 
     def month(self, execution_times, month_of_year):
@@ -25,8 +27,7 @@ class CloudCost():
               NOTE: The execution times is about how many executions was made per day
         '''
 
-        days = calendar.monthrange(datetime.datetime.now().year, month_of_year)[1]
-        return self.app_execution(execution_times * days)
+        return self.app_execution(execution_times) * calendar.monthrange(datetime.datetime.now().year, month_of_year)[1]
 
     def year(self, execution_times):
         ''' Returns a list with all total costs per month by a year '''
@@ -44,8 +45,4 @@ class CloudCost():
 
 if __name__ == '__main__':
     cc = CloudCost()
-    print(cc.year(1))
-    print(cc.year(50))
-    print(cc.year(100))
     print(cc.year(1000))
-    print(cc.year(5000))
