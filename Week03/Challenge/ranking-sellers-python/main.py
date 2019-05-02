bc38e891bf22c777e32a7406894797678e028abe
eric.dantas <ericrommel@gmail.com>
2019-05-02 11:49:53 +0200

    Improved using the methods that were aready implemented

--- Week03/Challenge/ranking-sellers-python/main.py
+++ Week03/Challenge/ranking-sellers-python/main.py
@@ -3,6 +3,9 @@ from operator import itemgetter
 class SellersRancking:
     def best_seller(self, sellers):
         ''' Mostrar o vendedor com maior valor em vendas. '''
+        # return [
+        #   max(sellers, key=itemgetter('value'))['name']
+        # ] if sellers else []
         return [
             sorted(sellers, key=itemgetter('value'), reverse=True)[0]['name']
         ] if sellers else []
@@ -10,23 +13,17 @@ class SellersRancking:
     def rancking_list(self, sellers):
         ''' Listar todos os vendedores ordenados pelo valor vendido, do maior para o menor '''
         return [
-            sellers['name'] for sellers in sorted(sellers, key=itemgetter('value'), reverse=True)
+            seller['name'] for seller in sorted(sellers, key=itemgetter('value'), reverse=True)
         ]
 
     def best_seller_store(self, sellers, store):
         ''' Mostrar o melhor vendedor de uma determinada loja. Você receberá a lista de vendedores e deverá fazer um
             filtro nesta lista pela loja e retornar o nome do vendedor com o maior valor em vendas.
         '''
-        return [
-            ([
-                sellers['name'] for sellers in sorted(sellers, key=itemgetter('value'), reverse=True) if sellers['store'] == store
-            ])[0]
-        ] if sellers else []
+        return self.best_seller([seller for seller in sellers if seller['store'] == store])
 
     def sales_goals(self, sellers):
         ''' Também é estipulado uma meta para vendas de R$ 500,00. Então é necessário listar os vendedores que não
             atingiram esta meta, em ordem crescente pelo valor de venda.
         '''
-        return [
-            sellers['name'] for sellers in sorted(sellers, key=itemgetter('value')) if sellers['value'] < 500
-        ]
+        return self.rancking_list([seller for seller in sellers if seller['value'] < 500])
