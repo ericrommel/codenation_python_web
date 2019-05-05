244e3744cc8ad7849bf977a41c30bf15ef6ebae5
eric.dantas <ericrommel@gmail.com>
2019-05-05 22:23:53 +0200

    Some improvements have been done.

--- Week03/Challenge/ranking-sellers-python/main.py
+++ Week03/Challenge/ranking-sellers-python/main.py
@@ -1,16 +1,13 @@
 from operator import itemgetter
 
-class SellersRancking:
+class SellersRanking:
     def best_seller(self, sellers):
         ''' Mostrar o vendedor com maior valor em vendas. '''
-        # return [
-        #   max(sellers, key=itemgetter('value'))['name']
-        # ] if sellers else []
         return [
             sorted(sellers, key=itemgetter('value'), reverse=True)[0]['name']
         ] if sellers else []
 
-    def rancking_list(self, sellers):
+    def ranking_list(self, sellers):
         ''' Listar todos os vendedores ordenados pelo valor vendido, do maior para o menor '''
         return [
             seller['name'] for seller in sorted(sellers, key=itemgetter('value'), reverse=True)
@@ -26,4 +23,6 @@ class SellersRancking:
         ''' Também é estipulado uma meta para vendas de R$ 500,00. Então é necessário listar os vendedores que não
             atingiram esta meta, em ordem crescente pelo valor de venda.
         '''
-        return self.rancking_list([seller for seller in sellers if seller['value'] < 500])
+        return [
+            seller['name'] for seller in sorted(sellers, key=itemgetter('value')) if seller['value'] < 500
+        ]
